c623aed59c4152a805577ca0fe6cd0d44453b902
Olumide Olowe <olumide@cretivestrides.com>
2025-09-27 21:54:39 +0100

    Fix javax.annotation -> jakarta.annotation for Spring Boot 3

--- src/main/java/com/example/transactionservice/TransactionServiceApplication.java
+++ src/main/java/com/example/transactionservice/TransactionServiceApplication.java
@@ -3,7 +3,8 @@ package com.example.transactionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Value;
-import javax.annotation.PostConstruct;
+//import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 /**
  * Entry point for the transaction service.
